1c5a95781ff86bec895292e22326136b57594e00
ICE <axvn@hotmail.com>
2022-01-29 20:15:31 +0100

    fix token uri

--- contracts/CyberPopBadge.sol
+++ contracts/CyberPopBadge.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 
 contract CyberPopBadge is
     Initializable,
@@ -15,23 +15,24 @@ contract CyberPopBadge is
     AccessControlUpgradeable,
     PausableUpgradeable,
     ERC1155BurnableUpgradeable,
-    ERC1155SupplyUpgradeable,
     UUPSUpgradeable
 {
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    string private uri_prefix;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     function initialize() public initializer {
-        __ERC1155_init("https://api.cyberpop.online/server/{}");
+        uri_prefix = "https://api.cyberpop.online/server/";
+        __ERC1155_init(uri_prefix);
+
         __AccessControl_init();
         __Pausable_init();
         __ERC1155Burnable_init();
-        __ERC1155Supply_init();
         __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -49,7 +50,23 @@ contract CyberPopBadge is
         return "CBG";
     }
 
+    function uri(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    uri_prefix,
+                    StringsUpgradeable.toString(_tokenId)
+                )
+            );
+    }
+
     function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        uri_prefix = newuri;
         _setURI(newuri);
     }
 
@@ -86,11 +103,7 @@ contract CyberPopBadge is
         uint256[] memory ids,
         uint256[] memory amounts,
         bytes memory data
-    )
-        internal
-        override(ERC1155Upgradeable, ERC1155SupplyUpgradeable)
-        whenNotPaused
-    {
+    ) internal override(ERC1155Upgradeable) whenNotPaused {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
