3333ce2a1ae7d5749dc8c5ff3e3bd4c140bba0ef
ICE <axvn@hotmail.com>
2022-02-15 15:12:39 +0100

    Switch to more granual access control

--- contracts/Cyborg.sol
+++ contracts/Cyborg.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
@@ -15,28 +15,30 @@ contract Cyborg is
     ERC721Upgradeable,
     ERC721EnumerableUpgradeable,
     ERC721BurnableUpgradeable,
-    OwnableUpgradeable,
+    AccessControlUpgradeable,
     UUPSUpgradeable
 {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
     string private _uriPrefix;
-    address private _minter;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     function __Cyborg_init() private {
-        _uriPrefix = "https://api.cyberpop.online/server/cyber/";
-        _minter = _msgSender();
+        _uriPrefix = "https://api.cyberpop.online/server/";
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
     }
 
     function initialize() public initializer {
         __ERC721_init("Cyborg", "CYBER");
         __ERC721Enumerable_init();
         __ERC721Burnable_init();
-        __Ownable_init();
+        __AccessControl_init();
         __UUPSUpgradeable_init();
 
         __Cyborg_init();
@@ -46,10 +48,6 @@ contract Cyborg is
         return _uriPrefix;
     }
 
-    function setMinter(address minter) public onlyOwner {
-        _minter = minter;
-    }
-
     function setURI(string memory newuri) public onlyOwner {
         _uriPrefix = newuri;
     }
@@ -57,10 +55,19 @@ contract Cyborg is
     /**
      * @dev Throws if called by any account other than the owner nor the minter.
      */
-    modifier mintable() {
+    modifier onlyMinter() {
         require(
-            owner() == _msgSender() || _minter == _msgSender(),
+            hasRole(MINTER_ROLE, _msgSender()) ||
+                hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
             "Cyborg: caller is not the owner nor the minter"
+        );
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Cyborg: caller is not admin"
         );
         _;
     }
@@ -80,9 +87,14 @@ contract Cyborg is
             );
     }
 
-    function safeMint(address to) public mintable {
+    function safeMint(address to) public onlyMinter {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // Allow specifying customized tokenId
+    function safeMint(address to, uint256 tokenId) public onlyMinter {
         _safeMint(to, tokenId);
     }
 
@@ -105,7 +117,11 @@ contract Cyborg is
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        override(
+            ERC721Upgradeable,
+            ERC721EnumerableUpgradeable,
+            AccessControlUpgradeable
+        )
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
