f8ba06b9312cbef8476ab368d43c960a77ce63f6
ICE <axvn@hotmail.com>
2022-03-07 17:32:32 +0100

    Fix specs

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -9,14 +9,21 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract TimeLock is Ownable {
     using SafeMath for uint256;
     address public token;
+
+    // Release every 30 days after the lock period
     uint256 public constant BATCH_PERIOD = 24 * 60 * 60 * 30;
 
     struct Locker {
+        // release cliff
         uint256 lockTimestamp;
+        // total locked amount per address
         uint256 lockedAmount;
+        // number of batches to release total locked amount
         uint256 releaseBatches;
-        uint256 releasedAmount;
+        // persist withdrawed amount up to date
+        uint256 withdrawedAmount;
     }
+
     // Locked amount and release time for each address
     mapping(address => Locker) public lockedBalances;
     uint256 public totalLocked;
@@ -35,7 +42,7 @@ contract TimeLock is Ownable {
             lockedBalances[msg.sender].lockTimestamp < block.timestamp,
             "CYT Locker: lock duration not passed"
         );
-        uint256 _amount = releaseableAmount(msg.sender);
+        uint256 _amount = releasedAmount(msg.sender);
         require(_amount > 0, "CYT Locker: insufficient balance");
 
         subBalance(msg.sender, _amount);
@@ -46,6 +53,10 @@ contract TimeLock is Ownable {
     /**
      * @notice Mint Token to this contract, mark the owner and lock period
      * @dev Can only be called by owner
+     * @param _sender The beneficient of the token
+     * @param _amount Total locked up amount
+     * @param _lockPeriod Locked period before the first release
+     * @param _batches Number of batches to release total amount
      */
     function lock(
         address _sender,
@@ -61,41 +72,47 @@ contract TimeLock is Ownable {
         CyberPopToken(token).mint(address(this), _amount);
         lockedBalances[_sender].lockedAmount = _amount;
         lockedBalances[_sender].releaseBatches = _batches;
-        lockedBalances[_sender].lockTimestamp = block.timestamp + _lockPeriod;
-        totalLocked = totalLocked + _amount;
+        lockedBalances[_sender].lockTimestamp = block.timestamp.add(
+            _lockPeriod
+        );
+        totalLocked = totalLocked.add(_amount);
     }
 
-    function subBalance(address _sender, uint256 _amount) private {
-        lockedBalances[_sender].releasedAmount = lockedBalances[_sender]
-            .releasedAmount
-            .add(_amount);
-        if (
-            lockedBalances[_sender].lockedAmount <=
-            lockedBalances[_sender].releasedAmount
-        ) {
-            delete lockedBalances[_sender]; // clean up storage for lockTimestamp
-        }
-        totalLocked = totalLocked - _amount;
-    }
-
-    function releaseableAmount(address _beneficient)
+    /**
+     * @notice Query released amount for the beneficient address
+     * @param _beneficient Address of the beneficient
+     */
+    function releasedAmount(address _beneficient)
         public
         view
         returns (uint256)
     {
         uint256 _now = block.timestamp;
         Locker memory locker = lockedBalances[_beneficient];
-        if (_now < locker.lockTimestamp) {
+        if (locker.lockTimestamp == 0 || _now < locker.lockTimestamp) {
             return 0;
         }
-        uint256 delta = _now.sub(lockedBalances[_beneficient].lockTimestamp);
-        uint256 batches = delta.div(BATCH_PERIOD);
-        if (batches > locker.releaseBatches) {
-            return locker.lockedAmount - locker.releasedAmount;
+        uint256 delta = _now.sub(locker.lockTimestamp);
+        uint256 batches = delta.div(BATCH_PERIOD) + 1; // starting from 1
+        if (batches >= locker.releaseBatches) {
+            return locker.lockedAmount - locker.withdrawedAmount;
         }
         return
-            locker.lockedAmount *
-            (batches / locker.releaseBatches) -
-            locker.releasedAmount;
+            (locker.lockedAmount * batches) /
+            locker.releaseBatches -
+            locker.withdrawedAmount;
+    }
+
+    function subBalance(address _sender, uint256 _amount) private {
+        lockedBalances[_sender].withdrawedAmount = lockedBalances[_sender]
+            .withdrawedAmount
+            .add(_amount);
+        if (
+            lockedBalances[_sender].lockedAmount <=
+            lockedBalances[_sender].withdrawedAmount
+        ) {
+            delete lockedBalances[_sender]; // clean up storage for lockTimestamp
+        }
+        totalLocked = totalLocked - _amount;
     }
 }
--- test/time_lock.js
+++ test/time_lock.js
@@ -1,64 +1,98 @@
 const TimeLock = artifacts.require("TimeLock")
 const CYT = artifacts.require("CyberPopToken")
-const { time, expectRevert } = require("@openzeppelin/test-helpers");
+const { time, expectRevert, snapshot } = require("@openzeppelin/test-helpers");
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
 
 contract("TimeLock", function (accounts) {
-  it("mints and lock for specified period", async function () {
-    let cyt = await CYT.deployed()
-    let timelock = await TimeLock.deployed()
+  let snapshotA
+  let cyt
+  let timelock
 
+  beforeEach(async () => {
+    cyt = await CYT.deployed()
+    timelock = await TimeLock.deployed()
+    snapshotA = await snapshot()
+  })
+
+  afterEach(async () => {
+    await snapshotA.restore()
+  })
+
+  it("mints and lock for specified period", async function () {
     let userA = accounts[1]
     let userB = accounts[2]
 
-    // release time
-    let due = new Date().getMilliseconds() + 60 * 1000
+    let batchPeriod = await timelock.BATCH_PERIOD()
+    let batches = 10
+    // start releasing in one year
+    let lockPeriod = batchPeriod.toNumber() * 12
+    let now = (await web3.eth.getBlock()).timestamp
+    let due = Math.floor(Date.now() / 1000) + lockPeriod
     // lock up 10 CYT
     let amount = 10_000_000
-    await timelock.lock(userA, amount, due)
+    await timelock.lock(userA, amount, lockPeriod, batches)
 
     let balance = await cyt.balanceOf(timelock.address)
     assert.equal(balance.toNumber(), amount)
 
-    await timelock.lock(userB, amount, due)
+    await timelock.lock(userB, amount, lockPeriod, batches)
     let totalLocked = await timelock.totalLocked()
 
     assert.equal(totalLocked.toNumber(), amount * 2)
 
     let locker = await timelock.lockedBalances(userA)
-    assert.equal(locker.lockTimestamp, due)
+    assert.equal(locker.releaseBatches, batches)
     assert.equal(locker.lockedAmount, amount)
 
+    assert.equal(locker.lockTimestamp.toNumber(), due)
+    // assert within 20 seconds
+    assert.isTrue(locker.lockTimestamp.toNumber() >= due && locker.lockTimestamp.toNumber() <= due + 100)
+
     // assert cannot change lock time
-    await expectRevert(timelock.lock(userA, amount, due), "CYT Locker: cannot re-lock a locked address")
+    await expectRevert(timelock.lock(userA, amount, lockPeriod, batches), "CYT Locker: cannot re-lock a locked address")
   })
 
   it("allows withdraw only after lock period", async function () {
-    let cyt = await CYT.deployed()
-    let timelock = await TimeLock.deployed()
-
     let userA = accounts[3]
     let userB = accounts[4]
-    console.log("userA: " + userA)
-    console.log("userB: " + userB)
 
-    // lock period
-    let lockPeriod = 60
+    let batchPeriod = await timelock.BATCH_PERIOD()
+    let batches = 5
+    // release time in 10 months
+    let lockPeriod = batchPeriod.toNumber() * 10
+    let now = (await web3.eth.getBlock()).timestamp
+    let due = Math.floor(Date.now() / 1000) + lockPeriod
+
     // lock up 10 CYT
     let amount = 10_000_000
-    await timelock.lock(userA, amount, lockPeriod, 10)
+    await timelock.lock(userA, amount, lockPeriod, batches)
 
-    await expectRevert(timelock.withdraw(amount, { from: userA }), "CYT Locker: lock duration not passed")
+    await expectRevert(timelock.withdraw({ from: userA }), "CYT Locker: lock duration not passed")
     // Cannot withdraw if there is no balance
-    await expectRevert(timelock.withdraw(amount, { from: userB }), "CYT Locker: insufficient balance")
+    await expectRevert(timelock.withdraw({ from: userB }), "CYT Locker: insufficient balance")
 
-    let locker = await timelock.lockedBalances(userA)
-    //assert.equal(locker.lockTimestamp, due)
-    console.log(locker.lockedAmount)
+    let released = await timelock.releasedAmount(userA)
 
+    await time.increaseTo(due + 100)
+    released = await timelock.releasedAmount(userA)
 
-    await time.increase(60 + 1)
-    // await timelock.withdraw(amount, { from: userA })
+    // release first batch
+    await timelock.withdraw({ from: userA })
     let balance = await cyt.balanceOf(userA)
+    assert.equal(balance.toNumber(), amount / batches)
+
+    // release second batch
+    await time.increase(batchPeriod)
+    await timelock.withdraw({ from: userA })
+    balance = await cyt.balanceOf(userA)
+    assert.equal(balance.toNumber(), (amount / batches) * 2)
+
+    // release all batches
+    await time.increase(batchPeriod * batches) // way over
+
+    await timelock.withdraw({ from: userA })
+    balance = await cyt.balanceOf(userA)
+    // balance should not exceed locked amount
     assert.equal(balance.toNumber(), amount)
   })
 })
