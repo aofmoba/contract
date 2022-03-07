e5f41a82254e031e9e925178bb9efdaf3cc6ffb5
ICE <axvn@hotmail.com>
2022-03-07 08:17:00 +0100

    Revert "Add CYT token"
    This reverts commit e586d04da2495023663d5904d22dca25bb099407.

--- migrations/4_deploy_cyt.js
+++
@@ -1,7 +0,0 @@
-const CYT = artifacts.require('CyberPopToken');
-
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
-module.exports = async function (deployer) {
-    await deployProxy(CYT, [], { deployer, kind: "uups" });
-};
