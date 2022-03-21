f58da8fff3d95a7b23547c4a12cae3dbb84f2029
ICE <axvn@hotmail.com>
2022-03-21 10:56:32 +0100

    Add ERC20VotesComp support

--- contracts/CyberPopToken.sol
+++ contracts/CyberPopToken.sol
@@ -6,12 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
 
 contract CyberPopToken is
     ERC20,
     ERC20Capped,
     ERC20Burnable,
     Pausable,
+    ERC20Permit,
+    ERC20VotesComp,
     AccessControl
 {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
@@ -20,6 +24,7 @@ contract CyberPopToken is
     constructor()
         ERC20("CyberPopToken", "CYT")
         ERC20Capped(120_000_000_000_000) // capped at 120m
+        ERC20Permit("CyberPopToken")
     {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
@@ -43,9 +48,11 @@ contract CyberPopToken is
         _mint(to, amount);
     }
 
+    // The following functions are overrides required by Solidity.
+
     function _mint(address to, uint256 amount)
         internal
-        override(ERC20, ERC20Capped)
+        override(ERC20, ERC20Capped, ERC20Votes)
     {
         super._mint(to, amount);
     }
@@ -56,5 +63,20 @@ contract CyberPopToken is
         uint256 amount
     ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
