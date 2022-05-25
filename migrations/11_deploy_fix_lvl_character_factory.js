603f9cf8efe2e821bf118f591a070f98fb9f0e68
ICE <btcerctrc@gmail.com>
2022-05-25 09:32:48 +0200

    Annotate the lootbox IDs

--- migrations/11_deploy_fix_lvl_character_factory.js
+++ migrations/11_deploy_fix_lvl_character_factory.js
@@ -19,11 +19,13 @@ module.exports = async (deployer, network, accounts) => {
 
     const lootbox = await LootBox.deployed()
     let optionId = await lootbox.numOptions()
+    // ID 7
     await lootbox.addNewOption(fixLvlCharFactory.address, [9000, 1000])
     console.log('Fixed level [9000, 1000] option ID:', optionId++)
-
+    // ID 8
     await lootbox.addNewOption(fixLvlCharFactory.address, [5000, 5000])
     console.log('Fixed level [5000, 5000] option ID:', optionId++)
+    // ID 9
     await lootbox.addNewOption(fixLvlCharFactory.address, [0, 800, 9000, 200])
     console.log('Fixed level [800, 9000, 200] option ID:', optionId++)
 
