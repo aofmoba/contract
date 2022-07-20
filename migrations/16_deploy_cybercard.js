a0877000be066e702fd989c18304dfc07ed0a550
chingfeng2021 <94507977+chingfeng2021@users.noreply.github.com>
2022-07-20 14:50:15 +0800

    Create 16_deploy_cybercard.js

---
+++ migrations/16_deploy_cybercard.js
@@ -0,0 +1,7 @@
+const { ChainIDPrefixes } = require("../lib/valuesCommon");
+const CyberCard = artifacts.require('CyberCard')
+
+module.exports = async function (deployer, network) {
+    // deploy CyberCard
+    await deployer.deploy(CyberCard, ChainIDPrefixes[network])
+};
